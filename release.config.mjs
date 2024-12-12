2172e2291dc30757295343e428fe62c5a608d9a2
Eric Yu <yyhurs@gmail.com>
2024-12-12 16:35:25 +0800

    install semantic-release

---
+++ release.config.mjs
@@ -0,0 +1,26 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ["master", "next"],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    // [
+    //   '@semantic-release/changelog',
+    //   {
+    //     changelogFile: 'docs/CHANGELOG.md',
+    //   },
+    // ],
+    // '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        // 寫在 assets 裡的內容可以在 CI 後被 commit 到 git repository 內
+        // 預設是 ['CHANGELOG.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json']
+        // assets: ['docs/CHANGELOG.md'],
+        "message": "chore(release): ${nextRelease.version} [skip ci]"
+      },
+    ],
+  ],
+};
