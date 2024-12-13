b53d1dd298885cc34d7ea5a4acd44e5dacc571c1
Eric Yu <yyhurs@gmail.com>
2024-12-13 14:45:19 +0800

    feat: 在 semantic-release config 補上 repositoryUrl，觀察是否能觸發自動修改 package.json version

--- release.config.mjs
+++ release.config.mjs
@@ -3,26 +3,7 @@
  */
 export default {
   branches: ["main"],
-  // plugins: [
-  //   '@semantic-release/commit-analyzer',
-  //   '@semantic-release/release-notes-generator',
-  //   // [
-  //   //   '@semantic-release/changelog',
-  //   //   {
-  //   //     changelogFile: 'docs/CHANGELOG.md',
-  //   //   },
-  //   // ],
-  //   // '@semantic-release/npm',
-  //   [
-  //     '@semantic-release/git',
-  //     {
-  //       // 寫在 assets 裡的內容可以在 CI 後被 commit 到 git repository 內
-  //       // 預設是 ['CHANGELOG.md', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json']
-  //       assets: ['docs/CHANGELOG.md'],
-  //       // "message": "chore(release): ${nextRelease.version} [skip ci]"
-  //     },
-  //   ],
-  // ],
+  repositoryUrl: "https://github.com/yyhurs/bump-version-test.git",
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
@@ -30,7 +11,7 @@ export default {
     [
       "@semantic-release/git",
       {
-        assets: ["package.json", "CHANGELOG.md"], // 指定要提交的檔案
+        assets: ["package.json", "CHANGELOG.md"],
         message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
